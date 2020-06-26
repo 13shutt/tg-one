f7689404b25122df88040afcdc575d3661363815
super pan <superpan@supers-iMac.local>
2020-06-26 13:14:26 +0300

    B: Added user's routes

---
+++ server/routes/users-routes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+const usersController = require('../controllers/users-controllers');
+
+// router.get('/', usersController.getUsers);
+
+router.get('/', usersController.getUserByUserName);
+
+router.get('/:uid', usersController.getUserById);
+
+router.post('/signup', usersController.signup);
+
+router.post('/login', usersController.login);
+
+module.exports = router;
