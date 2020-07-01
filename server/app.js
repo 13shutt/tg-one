149c56215e871828b616208448d58868d610f19a
super pan <superpan@supers-iMac.local>
2020-07-01 23:16:08 +0300

    B: Added Headers for CORS policy

--- server/app.js
+++ server/app.js
@@ -17,6 +17,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Static for images
 app.use('/uploads/images', express.static(path.join('uploads', 'images')));
 
+// Adding Headers for CORS policy
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers', 
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET', 'POST', 'PATCH', 'DELETE');
+
+    next();
+});
+
 // Main routes
 app.use('/api/users', usersRouter);
 
