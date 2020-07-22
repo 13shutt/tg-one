ae5392606dd586e25c057b6d45c55fc2edd15f79
super pan <superpan@supers-iMac.local>
2020-07-22 20:06:35 +0300

    - Fixed auth

--- server/middleware/auth-middleware.js
+++ server/middleware/auth-middleware.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 const config = require('config');
-const HttpError = require('../models/http-error');
+const HttpError = require('../helpers/http-error');
 
 module.exports = (req, res, next) => {
     if (req.method === 'OPTIONS') {
@@ -8,9 +8,9 @@ module.exports = (req, res, next) => {
     }
 
     try {
-        const auth = req.heareds.authorization;
+        const auth = req.headers.authorization;
 
-        if (!auth) { return next(new HttpError('Forbidden.', 403)); }
+        if(!auth) { return next(new HttpError('Forbidden.', 403)); }
 
         const token = auth.split(' ')[1];
 
@@ -19,6 +19,7 @@ module.exports = (req, res, next) => {
         const decoded = jwt.verify(token, config.get('jwtSecret'));
         req.user = decoded;
         next();
+
     } catch (err) {
         return next(new HttpError(err.message, 403));
     }
