9cebcf62dd0d381364ec001b01b169f82965db28
13shutt <shutt13a@gmail.com>
2020-08-02 22:26:41 +0300

    F: Add chat styling

--- client/src/view/Chats/Chats.js
+++ client/src/view/Chats/Chats.js
@@ -11,11 +11,19 @@ class Chats extends Component {
   render() {
     return (
       <section className="chats-section">
-        <div className="chats-list">
-          <ChatMin img={one} name="Rhino" />
-          <ChatMin img={two} name="Vulture" />
-          <ChatMin img={three} name="Electro" />
-          <ChatMin img={chotury} name="Green Goblin" />
+        <div className="chats">
+          <div className="chats-header">
+            <div className="chats-settings">
+              <i className="fas fa-bars"></i>
+            </div>
+            <input className="chats-search" type="text" placeholder="Search" />
+          </div>
+          <div className="chats-list">
+            <ChatMin img={one} name="Rhino" active="true" />
+            <ChatMin img={two} name="Vulture" />
+            <ChatMin img={three} name="Electro" />
+            <ChatMin img={chotury} name="Green Goblin" />
+          </div>
         </div>
         <div className="chat">
           <ChatMax name="Rhino" />
