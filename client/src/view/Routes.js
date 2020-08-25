5cc423206cbd866e3d943078c1063d0f18a81b39
13shutt <shutt13a@gmail.com>
2020-08-25 21:49:33 +0300

    F: Working MobX decorators

--- client/src/view/Routes.js
+++ client/src/view/Routes.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import { Helmet } from 'react-helmet'
 import { Route, Switch } from 'react-router-dom'
+import { inject, observer } from 'mobx-react'
 import Login from './Login'
 import Chats from './Chats'
 
 import api from 'api/index'
 
+@inject('routing')
+@observer
 export default class Routes extends Component {
   componentDidMount() {
     console.log('cdm')
