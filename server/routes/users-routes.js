4a1fab4c21a6e2481190df77ea80bc5fd1c67ee2
Bohdan Pantiukhov <58185639+gingermanymph@users.noreply.github.com>
2020-09-14 15:31:00 +0300

    B: Added user's pagination

--- server/routes/users-routes.js
+++ server/routes/users-routes.js
@@ -4,12 +4,14 @@ const router = express.Router();
 const usersController = require('../controllers/users-controllers');
 const fileUpload = require('../middleware/file-upload');
 const authMiddleware = require('../middleware/auth-middleware');
+const pagination = require('../middleware/pagination-middleware');
+const User = require('../models/user');
 
-router.get('/', usersController.getUserByUserName);
+router.get('/', authMiddleware, usersController.getUserByUserName);
 
-router.get('/', usersController.getUsers);
+router.get('/', authMiddleware, pagination(User), usersController.getUsers);
 
-router.get('/:uid', usersController.getUserById);
+router.get('/:uid', authMiddleware, usersController.getUserById);
 
 router.post('/signup',
     fileUpload.single('image'),
