14957cce979ec3668863efce92f30a86fd729ff8
13shutt <shutt13a@gmail.com>
2020-10-15 12:31:34 +0300

    F: Add more styles

--- client/src/components/ChatMax/ChatMax.js
+++ client/src/components/ChatMax/ChatMax.js
@@ -6,7 +6,7 @@ const ChatMax = (props) => (
     <div className="chatMax-header">
       <div className="chatMax-header-user">
         <div>{props.name}</div>
-        <span>online</span>
+        <span>online (or not)</span>
       </div>
       <div className="chatMax-header-info">
         <div>
@@ -18,7 +18,12 @@ const ChatMax = (props) => (
       </div>
     </div>
     <div className="chatMax-main"></div>
-    <div className="chatMax-footer"></div>
+    <div className="chatMax-footer">
+      <input type="text" placeholder="Write a message..."/>
+      <div>
+        <i class="fa fa-rocket" aria-hidden="true"></i>
+      </div>
+    </div>
   </div>
 )
 export default ChatMax
